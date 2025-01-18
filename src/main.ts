1df69547b943a651d734ea9a87aca14e8fa7d43a
ezTxmMC <eztxmmc@hotmail.com>
2025-01-18 10:44:01 +0100

    optimizes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 app.use(express.json());
 
@@ -9,6 +9,6 @@ app.get("/", (request: Request, response: Response) => {
   response.send("Hello, Express with TypeScript!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
